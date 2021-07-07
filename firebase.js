8c151f1ee684d48696d8ae3eaf5f42d1af01c20a
Samuel Osei Adu <asamuel355@yahoo.com>
2021-07-07 12:19:43 +0000

    new update

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAkig5c6KEgfK5XiVSxrMX_t4YY9yqwN2A",
+    authDomain: "netflixapp-65ba0.firebaseapp.com",
+    projectId: "netflixapp-65ba0",
+    storageBucket: "netflixapp-65ba0.appspot.com",
+    messagingSenderId: "304209194902",
+    appId: "1:304209194902:web:f5aae9c6dd2f19df6c77b7",
+    measurementId: "G-3VRS9RCQ4V"
+  };
