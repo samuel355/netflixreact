cdc5f4569759d498faaef27e72c5a5f2bbb15de6
Samuel Osei Adu <asamuel355@yahoo.com>
2021-07-08 01:26:46 +0000

    new update

---
+++ src/screens/HomeScreen/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img src="./images/netflix-logo.png" alt="" />
+
+      <img src="./images/netflix-user.png" alt="" />
+    </div>
+  );
+}
+
+export default Nav;
